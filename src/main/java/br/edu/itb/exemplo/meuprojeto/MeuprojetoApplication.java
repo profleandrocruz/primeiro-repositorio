aef8457c3c1f1873fc53a76c45ad58d6baaf695b
Leandro Cruz <leandrocruzprofitb@gmail.com>
2022-03-02 14:59:10 -0300

    Primeiro Commit

---
+++ src/main/java/br/edu/itb/exemplo/meuprojeto/MeuprojetoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.itb.exemplo.meuprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/itb/exemplo/meuprojeto/MeuprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.itb.exemplo.meuprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
